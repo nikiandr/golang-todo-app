6729f2c1761b493c2c877b50a77255f0b04f2e75
Nikita Fordui <nikita.forduy@protonmail.com>
2021-09-19 18:41:20 +0300

    endpoint groups + empty handler for each endpoint created

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"github.com/nikiandr/golang-todo-app"
+	"github.com/nikiandr/golang-todo-app/pkg/handler"
 	"log"
 )
 
 func main() {
+	handlers := new(handler.Handler)
+
 	srv := new(todo.Server)
-	if err := srv.Run("8080"); err != nil {
+	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
 		log.Fatalf("Error occured while running http server: %s", err.Error())
 	}
 }
