f24e5a3fa12c01145d26947a3d8bd0a009181c74
Nikita Fordui <nikita.forduy@protonmail.com>
2021-09-20 12:13:10 +0300

    database files for postgres added

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"github.com/nikiandr/golang-todo-app"
 	"github.com/nikiandr/golang-todo-app/pkg/handler"
 	"github.com/nikiandr/golang-todo-app/pkg/repository"
 	"github.com/nikiandr/golang-todo-app/pkg/service"
 	"github.com/spf13/viper"
 	"log"
+	"os"
 )
 
 func initConfig() error {
@@ -17,10 +20,27 @@ func initConfig() error {
 
 func main() {
 	if err := initConfig(); err != nil {
-		log.Fatalf("Error reading configs: %s", err.Error())
+		log.Fatalf("Error reading YAML configs: %s", err.Error())
 	}
 
-	repos := repository.NewRepository()
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Error reading .env configs: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+	})
+
+	if err != nil {
+		log.Fatalf("Error occured while connecting to DB server: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
